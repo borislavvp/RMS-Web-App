7a5bccd7d43d6112c6287b089ebb5bacbbbca255
Lyubomir <lyubomir555@gmail.com>
2021-03-13 19:59:35 +0100

    Added some components
    Home, header and footer components, global styling added

--- website/src/app/app.component.ts
+++ website/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'website';
+  title = "ChorbaDeck";
+
+  constructor(private router: Router,
+              private titleService: Title,
+              private activatedRoute: ActivatedRoute) { }
+
+  ngOnInit() {
+    const appTitle = this.titleService.getTitle();
+    this.router
+      .events.pipe(
+        filter(event => event instanceof NavigationEnd),
+        map(() => {
+          const child = this.activatedRoute.firstChild;
+          if (child.snapshot.data['title']) {
+            return child.snapshot.data['title'];
+          }
+          return appTitle;
+        })
+      ).subscribe((ttl: string) => {
+        this.titleService.setTitle(ttl);
+      });
+  }
 }
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -1,16 +1,33 @@
+// * General Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+
+// * Angular Material
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+
+// * Other Modules
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// * Components
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
