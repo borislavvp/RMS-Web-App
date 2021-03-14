f39156080bb61e79681e5806f9bfdf34402418ae
Lyubomir <lyubomir555@gmail.com>
2021-03-14 14:19:36 +0100

    Website home page

--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -15,13 +15,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
