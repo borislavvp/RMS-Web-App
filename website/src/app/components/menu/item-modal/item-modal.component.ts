91b02353b904bc2aa4eeb67e4395eee7f43c8ad3
Lyubomir <lyubomir555@gmail.com>
2021-05-19 16:00:50 +0200

    adding items from item modal

--- website/src/app/components/menu/item-modal/item-modal.component.ts
+++ website/src/app/components/menu/item-modal/item-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { BasketItem } from 'src/app/models/basket/basketItem.model';
 import { Product } from 'src/app/models/product.model';
+import { BasketService } from 'src/app/services/basket.service';
 
 @Component({
   selector: 'app-item-modal',
@@ -12,7 +14,8 @@ export class ItemModalComponent implements OnInit {
   product: Product;
 
   constructor(@Inject(MAT_DIALOG_DATA) public data,
-    private dialogRef: MatDialogRef<ItemModalComponent>) { }
+    private dialogRef: MatDialogRef<ItemModalComponent>,
+    private basketService: BasketService) { }
 
   ngOnInit(): void {
     this.product = this.data.product;
@@ -22,6 +25,13 @@ export class ItemModalComponent implements OnInit {
     this.dialogRef.close();
   }
 
-  addToBasket(){}
+  addToBasket(){
+    var item: BasketItem = {
+      productId: this.product.id,
+      productName: this.product.name,
+      price: this.product.price
+    }
+    this.basketService.addItem(item);
+  }
 
 }
