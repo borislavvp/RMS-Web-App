0486a310ac36e072a78ba5c082c147cde70182e6
Lyubomir <lyubomir555@gmail.com>
2021-05-30 08:43:53 +0200

    config and styling

--- website/src/app/components/account/account.component.ts
+++ website/src/app/components/account/account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Order } from 'src/app/models/order/order.model';
 
 @Component({
   selector: 'app-account',
@@ -7,11 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AccountComponent implements OnInit {
 
-  status = 1;
+  order: Order = { id: 1, status: "new" }
+
+  trackerStates: string[];
+  statusIcons: string[];
+  orderNameStatus: string[] = ["Accepted", "Cooking", "Packaging", "Delivering", "Delivered"];
 
   constructor() { }
 
   ngOnInit(): void {
+    if(this.order.status == "new"){
+      this.trackerStates = ["current-state", "next-state", "next-state", "next-state", "next-state"];
+      this.statusIcons = ["accepted_current.png", "cooking_future.png", "packaging_future.png", "delivering_future.png", "delivered_future.png"];
+    }
+    if(this.order.status == "preparing"){
+      this.trackerStates = ["previous-state", "current-state", "next-state", "next-state", "next-state"];
+      this.statusIcons = ["accepted_past.png", "cooking_current.png", "packaging_future.png", "delivering_future.png", "delivered_future.png"];
+    }
+    if(this.order.status == "prepared"){
+      this.trackerStates = ["previous-state", "previous-state", "current-state", "next-state", "next-state"];
+      this.statusIcons = ["accepted_past.png", "cooking_past.png", "packaging_current.png", "delivering_future.png", "delivered_future.png"];
+    }
+    if(this.order.status == "delivering"){
+      this.trackerStates = ["previous-state", "previous-state", "previous-state", "current-state", "next-state"];
+      this.statusIcons = ["accepted_past.png", "cooking_past.png", "packaging_past.png", "delivering_current.png", "delivered_future.png"];
+    }
+    if(this.order.status == "delivered"){
+      this.trackerStates = ["previous-state", "previous-state", "previous-state", "previous-state", "done"];
+      this.statusIcons = ["accepted_past.png", "cooking_past.png", "packaging_past.png", "delivering_past.png", "delivered_current.png"];
+    }
   }
 
 }
